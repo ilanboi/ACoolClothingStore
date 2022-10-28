c755f9537aee523c37cf3910bf2c3021efd7d9da
shellybrenner <shelly976@gmail.com>
2022-10-28 23:45:18 +0300

    creating contact css

---
+++ views/js/handlers/cookiesHandler.js
@@ -0,0 +1,29 @@
+function addToCookies(cookie_key, cookie_val, exp) {
+    document.cookie = `${cookie_key}=${cookie_val}; expires=${exp}; path=/;`;
+}
+
+function deleteCookie(cookie_key) {
+    document.cookie = `${cookie_key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
+
+function getCookie(cookie_key) {
+    let name = cookie_key + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+function addYears(date, years) {
+    date.setFullYear(date.getFullYear() + years);
+    return date;
+}
